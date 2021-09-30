816e518070a7ad5c0f5cdd3c79d5834b14fd1a28
Alex1505Gold <49805582+Alex1505Gold@users.noreply.github.com>
2021-09-30 18:57:32 +0300

    Add files via upload

--- Dual.cpp
+++ Dual.cpp
@@ -1,15 +1,39 @@
-﻿#include <iostream>
+﻿#include <iomanip>
+#include <iostream>
 #include <fstream>
 
 //key for min = 1, key for max = -1
 
 void simplex(double s_matrix[4][4], int key)
 {
-    int basis[3] = { 1 , 2, 3 };
-    int free[3] = { 4, 5, 6 };
-    double ad_matrix[4][4];
-    bool f = true;
+    const unsigned short int basis_size = 3;
+    const unsigned short int matrix_length = 4;
+    const unsigned short int matrix_width = 4;
+
+    double ad_matrix[matrix_length][matrix_width];
+
+    int basis[basis_size] = { 1 , 2, 3 };
+    int free[basis_size] = { 4, 5, 6 };
     double min = -1;
+    std::cout << std::fixed;
+    std::cout << "        s0         ";
+    for (int i = 0; i < basis_size; i++)
+    {
+        std::cout << "x" << i + 1 << "          ";
+    }
+    std::cout << "\n";
+    for (int i = 0; i < matrix_length; ++i)
+    {
+        if (i <= 2) std::cout << "x" << i + 3 << "  ";
+        else std::cout << "F   ";
+        for (int j = 0; j < matrix_width; ++j)
+        {
+            std::cout << std::setw(8) << std::setprecision(3) << s_matrix[i][j] << "   ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    bool f = true;
     int perm_col, perm_str;
     bool exist = true;
     while (f && exist)
@@ -17,7 +41,7 @@ void simplex(double s_matrix[4][4], int key)
         perm_str = -1;
         perm_col = -1;
 
-        for (int i = 0; i < 3; ++i)
+        for (int i = 0; i < matrix_length - 1; ++i)
         {
             if (s_matrix[i][0] < 0)
             {
@@ -28,7 +52,7 @@ void simplex(double s_matrix[4][4], int key)
         if (perm_str >= 0)
         {
             exist = false;
-            for (int i = 1; i < 4; ++i)
+            for (int i = 1; i < matrix_width; ++i)
             {
                 if (s_matrix[perm_str][i] < 0)
                 {
@@ -39,7 +63,7 @@ void simplex(double s_matrix[4][4], int key)
             }
             if (exist)
             {
-                for (int i = 0; i < 3; ++i)
+                for (int i = 0; i < matrix_length - 1; ++i)
                 {
                     if (((s_matrix[i][0] / s_matrix[i][perm_col]) >= 0) && ((min < 0) || (s_matrix[i][0] / s_matrix[i][perm_col] < min)))
                     {
@@ -47,9 +71,9 @@ void simplex(double s_matrix[4][4], int key)
                         perm_str = i;
                     }
                 }
-                for (int i = 0; i < 4; i++)
+                for (int i = 0; i < matrix_length; i++)
                 {
-                    for (int j = 0; j < 4; j++)
+                    for (int j = 0; j < matrix_width; j++)
                     {
                         if (i != perm_str && j != perm_col)
                             ad_matrix[i][j] = s_matrix[i][j] - s_matrix[i][perm_col] * s_matrix[perm_str][j] / s_matrix[perm_str][perm_col];
@@ -59,33 +83,33 @@ void simplex(double s_matrix[4][4], int key)
                             ad_matrix[i][j] = s_matrix[i][j] / s_matrix[perm_str][perm_col];
                         else
                             ad_matrix[i][j] = 1 / s_matrix[i][j];
+                        if (abs(ad_matrix[i][j]) <= pow(10, -12)) ad_matrix[i][j] = abs(ad_matrix[i][j]);
                     }
                 }
-                for (int i = 0; i < 4; ++i)
-                    for (int j = 0; j < 4; ++j)
+                for (int i = 0; i < matrix_length; ++i)
+                    for (int j = 0; j < matrix_width; ++j)
                         s_matrix[i][j] = ad_matrix[i][j];
                 std::swap(basis[perm_col - 1], free[perm_str]);
-                std::cout << "   s0  ";
-                for (int i = 0; i < 3; i++)
+                std::cout << "         s0         ";
+                for (int i = 0; i < basis_size; i++)
                 {
-                    std::cout << "x" << basis[i] << "  ";
+                    std::cout << "x" << basis[i] << "         ";
                 }
                 std::cout << "\n";
-                for (int i = 0; i < 4; ++i)
+                for (int i = 0; i < matrix_length; ++i)
                 {
                     if (i <= 2) std::cout << "x" << free[i] << "  ";
                     else std::cout << "F   ";
-                    for (int j = 0; j < 4; ++j)
+                    for (int j = 0; j < matrix_width; ++j)
                     {
-                        std::cout << s_matrix[i][j] << "   ";
+                        std::cout << std::setw(8) << std::setprecision(3) << s_matrix[i][j] << "   ";
                     }
                     std::cout << std::endl;
                 }
+                std::cout << std::endl;
             }
-            
         }
-        else
-            f = false;
+        else f = false;
     }
     if (exist) f = true;
     else std::cout << "There is no solution\n";
@@ -94,9 +118,9 @@ void simplex(double s_matrix[4][4], int key)
     {
         perm_col = -1;
         min = -1;
-        for (int i = 1; i < 4; ++i)
+        for (int i = 1; i < matrix_width; ++i)
         {
-            if (s_matrix[3][i] > 0)
+            if (s_matrix[matrix_length - 1][i] > 0)
             {
                 perm_col = i;
                 break;
@@ -105,7 +129,7 @@ void simplex(double s_matrix[4][4], int key)
         if (perm_col > 0)
         {
             unlim = true;
-            for (int i = 0; i < 3; ++i)
+            for (int i = 0; i < matrix_length - 1; ++i)
                 if (s_matrix[i][perm_col] > 0)
                 {
                     unlim = false;
@@ -113,7 +137,7 @@ void simplex(double s_matrix[4][4], int key)
                 }
             if (!unlim)
             {
-                for (int i = 0; i < 3; ++i)
+                for (int i = 0; i < matrix_length - 1; ++i)
                 {
                     if (((s_matrix[i][0] / s_matrix[i][perm_col]) >= 0) && ((min < 0) || (s_matrix[i][0] / s_matrix[i][perm_col] < min)))
                     {
@@ -121,9 +145,9 @@ void simplex(double s_matrix[4][4], int key)
                         perm_str = i;
                     }
                 }
-                for (int i = 0; i < 4; i++)
+                for (int i = 0; i < matrix_length; i++)
                 {
-                    for (int j = 0; j < 4; j++)
+                    for (int j = 0; j < matrix_width; j++)
                     {
                         if (i != perm_str && j != perm_col)
                             ad_matrix[i][j] = s_matrix[i][j] - s_matrix[i][perm_col] * s_matrix[perm_str][j] / s_matrix[perm_str][perm_col];
@@ -133,28 +157,31 @@ void simplex(double s_matrix[4][4], int key)
                             ad_matrix[i][j] = s_matrix[i][j] / s_matrix[perm_str][perm_col];
                         else
                             ad_matrix[i][j] = 1 / s_matrix[i][j];
+
+                        if (abs(ad_matrix[i][j]) <= pow(10, -12)) ad_matrix[i][j] = abs(ad_matrix[i][j]);
                     }
                 }
-                for (int i = 0; i < 4; ++i)
-                    for (int j = 0; j < 4; ++j)
+                for (int i = 0; i < matrix_length; ++i)
+                    for (int j = 0; j < matrix_width; ++j)
                         s_matrix[i][j] = ad_matrix[i][j];
                 std::swap(basis[perm_col - 1], free[perm_str]);
-                std::cout << "   s0  ";
-                for (int i = 0; i < 3; i++)
+                std::cout << "         s0         ";
+                for (int i = 0; i < basis_size; i++)
                 {
-                    std::cout << "x" << basis[i] << "  ";
+                    std::cout << "x" << basis[i] << "         ";
                 }
                 std::cout << "\n";
-                for (int i = 0; i < 4; ++i)
+                for (int i = 0; i < matrix_length; ++i)
                 {
                     if (i <= 2) std::cout << "x" << free[i] << "  ";
                     else std::cout << "F   ";
-                    for (int j = 0; j < 4; ++j)
+                    for (int j = 0; j < matrix_width; ++j)
                     {
-                        std::cout << s_matrix[i][j] << "   ";
+                        std::cout << std::setw(8) << std::setprecision(3) << s_matrix[i][j] << "   ";
                     }
                     std::cout << std::endl;
                 }
+                std::cout << std::endl;
             }
         }
         else f = false;
@@ -167,80 +194,68 @@ void simplex(double s_matrix[4][4], int key)
     else
     {
         std::cout << "\nSolution:\n";
-        std::cout << "F = " << s_matrix[3][0] * key << "\n";
-        for (int i = 0; i < 3; ++i)
+        std::cout << "F  = " << s_matrix[matrix_length - 1][0] * key << "\n";
+        for (int i = 0; i < basis_size; ++i)
         {
-            std::cout << "x" << basis[i] << " = 0\n";
+            if (basis[i] <= basis_size)
+                std::cout << "x" << basis[i] << " = 0\n";
         }
-        for (int i = 0; i < 3; ++i)
+        for (int i = 0; i < basis_size; ++i)
         {
-            std::cout << "x" << free[i] << " = " << s_matrix[i][0] << "\n";
+            if (free[i] <= basis_size)
+                std::cout << "x" << free[i] << " = " << s_matrix[i][0] << "\n";
         }
     }
 }
 
-int main()
+void dual_method()
 {
-    double s_matrix[4][4];
-    double d_matrix[4][4];
+    const unsigned short int basis_size = 3;
+    const unsigned short int matrix_length = 4;
+    const unsigned short int matrix_width = 4;
+    double s_matrix[matrix_length][matrix_width];
+    double d_matrix[matrix_length][matrix_width];
     std::ifstream in("input.txt");
-    for (int i = 0; i < 3; ++i)
-        in >> s_matrix[3][i + 1];
+    for (int i = 0; i < matrix_width - 1; ++i)
+        in >> s_matrix[matrix_length - 1][i + 1];
 
-    for (int i = 0; i < 3; ++i)
-        for (int j = 0; j < 3; ++j)
+    for (int i = 0; i < matrix_length - 1; ++i)
+        for (int j = 0; j < matrix_width - 1; ++j)
         {
             in >> s_matrix[i][j + 1];
         }
-    for (int i = 0; i < 3; ++i)
+    for (int i = 0; i < matrix_length - 1; ++i)
         in >> s_matrix[i][0];
-    s_matrix[3][0] = 0;
-    std::cout << "   s0  ";
-    for (int i = 0; i < 3; i++)
-    {
-        std::cout << "x" << i + 1 << "  ";
-    }
-    std::cout << "\n";
-    for (int i = 0; i < 4; ++i)
-    {
-        if (i <= 2) std::cout << "x" << i + 3 << "  ";
-        else std::cout << "F   ";
-        for (int j = 0; j < 4; ++j)
-        {
-            std::cout << s_matrix[i][j] << "   ";
-        }
-        std::cout << std::endl;
-    }
+    s_matrix[matrix_length - 1][0] = 0;
     in.close();
     std::cout << std::endl;
-    for (int i = 0; i < 4; i++)
-        for (int j = 1; j < 4; j++)
-            d_matrix[j - 1][i + 1] = -1 * s_matrix[i][j];
-    for (int i = 0; i < 3; i++)
-        d_matrix[i][0] = -1 * s_matrix[3][i + 1];
-    for (int i = 0; i < 3; i++)
-        d_matrix[3][i + 1] = -1 * s_matrix[i][0];
-    d_matrix[3][0] = 0;
-    std::cout << "   s0  ";
-    for (int i = 0; i < 3; i++)
-    {
-        std::cout << "x" << i + 1 << "  ";
-    }
-    std::cout << "\n";
-    for (int i = 0; i < 4; ++i)
-    {
-        if (i <= 2) std::cout << "x" << i + 3 << "  ";
-        else std::cout << "F   ";
-        for (int j = 0; j < 4; ++j)
+    for (int i = 0; i < matrix_length; i++)
+        for (int j = 1; j < matrix_width; j++)
         {
-            std::cout << d_matrix[i][j] << "   ";
+            d_matrix[j - 1][i + 1] = -1 * s_matrix[i][j];
+            if (d_matrix[j - 1][i + 1] == 0) d_matrix[j - 1][i + 1] = abs(d_matrix[j - 1][i + 1]);
         }
-        std::cout << std::endl;
+    for (int i = 0; i < matrix_length - 1; i++)
+    {
+        d_matrix[i][0] = -1 * s_matrix[matrix_length - 1][i + 1];
+        if (d_matrix[i][0] == 0) d_matrix[i][0] = abs(d_matrix[i][0]);
     }
-    std::cout << "--------------------------------------------\n";
+    for (int i = 0; i < matrix_width - 1; i++)
+    {
+        d_matrix[matrix_length - 1][i + 1] = -1 * s_matrix[i][0];
+        if (d_matrix[matrix_length - 1][i + 1] == 0) d_matrix[matrix_length - 1][i + 1] = abs(d_matrix[matrix_length - 1][i + 1]);
+    }
+    d_matrix[matrix_length - 1][0] = 0;
+    
+    std::cout << "--------------------Direct-task---------------\n\n";
     simplex(s_matrix, -1);
-    std::cout << "---------------------------------------------\n";
+    std::cout << "---------------------Dual-task----------------\n\n";
     simplex(d_matrix, 1);
+}
+
+int main()
+{
+    dual_method();
     return 0;
 }
 
